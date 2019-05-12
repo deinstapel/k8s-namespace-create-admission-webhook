d56104606b22048bf622c1e994e75226c25fcbe1
Johann Wagner <wagner@creatdev-solutions.com>
2019-05-12 22:23:17 +0200

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM node:11
+
+COPY . /k8s-namespace-create-admission-webhook
+WORKDIR /k8s-namespace-create-admission-webhook
+
+RUN npm ci
+
+CMD ["node", "/k8s-namespace-create-admission-webhook/src/index.js"]
---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const answerCall = (isAllowed, res) => {
+    res.json({
+        allowed: isAllowed,
+    });
+};
+
+app.use(bodyParser.json());
+
+app.post('/validateNamespaceCreation', (req, res) => {
+    if (req.body.operation !== 'CREATE') {
+        return answerCall(true, res);
+    }
+
+    const user = req.body.userInfo.Username.substr(10);
+    const createdNamespaceName = req.body.metadata.name;
+
+    const isAllowed = createdNamespaceName.startsWith(`${user}-`);
+    console.log('user', user);
+    console.log('namespace', createdNamespaceName);
+    console.log('isAllowed', isAllowed);
+
+    return answerCall(isAllowed, res);
+});
+
+app.listen(3000);
+console.log('Listening on port 3000');
